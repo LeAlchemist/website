de89895e3e02cd6811ccff38aadfd49abd131c77
Nicholas Ross <RossNicholasV@gmail.com>
2024-09-05 21:24:35 +0000

    removed old site files

--- old/header.js
+++
@@ -1,8 +0,0 @@
-fetch('header.html')
-    .then(res => res.text())
-    .then(text => {
-        let oldelem = document.querySelector("script#replace_with_header");
-        let newelem = document.createElement("div");
-        newelem.innerHTML = text;
-        oldelem.parentNode.replaceChild(newelem, oldelem);
-    })
