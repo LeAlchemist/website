a85bbc50f748cf5730b57b5a0d14b795efd0d704
Nicholas Ross <RossNicholasV@gmail.com>
2024-09-06 21:39:03 +0000

    First Blog Post

--- src/routes/blog/data.js
+++ src/routes/blog/data.js
@@ -1,7 +1,16 @@
 export const posts = [{
-    slug: 'test',
-    title: 'This is a test',
-    date: '8/27/2024',
+    //#region First Blog post, "What is this?"
+    slug: '09062024',
+    title: 'What is this?',
+    date: '9/6/2024',
     content:
-        '<p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus eleifend, nisi at euismod hendrerit, risus dolor varius risus, sed faucibus metus mauris in lacus. Nam sed nunc vel est bibendum commodo. Pellentesque pretium nulla nec ex fermentum pretium. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Morbi posuere nibh id ipsum feugiat, eget aliquam velit ullamcorper. Vivamus dignissim efficitur turpis, eu blandit lorem. Ut odio purus, mattis sit amet convallis id, ultrices sit amet turpis. Vivamus convallis erat ac finibus pellentesque. Nam tristique maximus mi, eget auctor arcu tristique non. Curabitur a leo ligula. In fringilla efficitur pretium. Vestibulum vehicula pretium finibus.</p><p>Nullam odio justo, tincidunt eu ante ac, pulvinar iaculis nunc.Curabitur laoreet nibh eget felis mattis, placerat elementum lorem rutrum.Integer posuere neque ligula, vitae lacinia ipsum facilisis vitae.Morbi porta justo ac metus ornare, vel aliquet urna euismod.Suspendisse posuere mi orci, id varius nisl accumsan non.Donec ac ante et odio imperdiet laoreet id vel nulla.Phasellus semper, turpis condimentum commodo volutpat, sapien metus vehicula ex, sed maximus odio mi at dolor.Praesent quis nisl sed ligula sollicitudin hendrerit a id lacus.Integer tempor consequat nisi sit amet tempus.Sed pulvinar nibh sit amet feugiat lobortis.Quisque pulvinar, eros placerat pharetra ornare, neque mi rutrum lectus, at rutrum nisl felis et ipsum.Vestibulum sit amet urna purus.Aenean luctus hendrerit augue, ac feugiat lectus pharetra eu.Integer vel erat a ipsum tempor facilisis eu sit amet nibh.</p>',
+        '<p>Hey look at this, this is the very first post on my website, kinda awesome right? To be absolutely honest, I doubt this post will see the light of day when the site is released, but I wanted to at the very least have something here to test the blog section of my site properly before I go and finalize the purchase of a domain and set up a Cloudflare account (as of writing this, I\'ve already set up a Cloudflare account). Before I get ahead of my self, I should start by stating who I am and what the point of this website.</p>' +
+        '<p><h3>Who am I</h3></p>' +
+        '<p>Hi, I am <b>Nicholas Ross</b>, I am a 3D artist, texture designer, and game designer with several years of experience working in freelance designing background props for indie games and animations, along with helping teams prototype gameplay systems and non-programmer oriented systems for writing and design users in Unity 3D. I also have interests in software development using Python, C++, and C#, along with playing with micro controllers for fun DIY projects and simple IoT projects using MicroPython and CircuitPython.</p>' +
+        '<p><h3>Why did I create this site</h3></p>' +
+        '<p>I created this site as an excuse to refresh my knowledge of web design while learning responsive web design techniques using Svelte and Svelte kit.</p>' +
+        '<p><h3>What are my plans</h3></p>' +
+        '<p>Currently my plans for this site, is to use it as a portfolio for design projects, a place to write about projects that I am doing, be it software, game design, or even some super nerdy physical projects. As of writing this, I do not have any set plans for how often I want to release new content to this site, I currently want to take some time to finalize the basic design of the site, and even update past projects so that I can list them here.</p>' +
+        '<p>If you read all of this, I would like to say thanks, and I cant wait to share more in the future.</p>'
+    //#endregion
 }];
