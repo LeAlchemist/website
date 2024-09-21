a6dbcca1fc2f41db416f09c5d93a8cec86eed0b5
Nicholas Ross <RossNicholasV@gmail.com>
2024-09-21 02:58:34 +0000

    Removed +layout.svelte to fix filtering

--- src/routes/portfolio/+page.svelte
+++ src/routes/portfolio/+page.svelte
@@ -1,13 +1,44 @@
 <script>
-	import img_tag from './+layout.svelte'
+	/**import img_tag from './+layout.svelte'
+
+	/**
+     * @param {string} img_tag
+     */
+	export let img_tag = "";
+	/**
+     * @param {string} _tag
+     */
+	function imgtag(_tag){
+		img_tag = _tag;
+		
+	}
 
 	export let data;
 </script>
 
+<!--#region filter bar-->
+<div class= "w-[100vw] m-auto bg-[#333]">
+	<div class= "text-white text-[20px] text-center">
+		<ul class = "list-none m-0 p-0 overflow-hidden w-[50vw]">
+			<li class= "float-left">Filters:</li>
+			<li class= "float-left">
+				<button class="bg-[#333] border-none color-white text-center text-[100%] text-none min-w-[100px] cursor-pointer hover:bg-[#111]" on:click={() => imgtag("")}>View All</button>
+			</li>
+			<li class= "float-left">
+				<button class="bg-[#333] border-none color-white text-center text-[100%] text-none min-w-[100px] cursor-pointer hover:bg-[#111]" on:click={() => imgtag("3D")}>View 3D</button>
+			</li>
+			<li class= "float-left">
+				<button class="bg-[#333] border-none color-white text-center text-[100%] text-none min-w-[100px] cursor-pointer hover:bg-[#111]" on:click={() => imgtag("2D")}>View2D</button>
+			</li>
+		</ul>
+	</div>
+</div>
+<!--#endregion-->
+
 <!--#region Images-->
 	<div class = "flex flex-wrap ">
 		{#each data.summaries as {tag, slug, image, title}}
-			{#if tag.includes("")}
+			{#if tag.includes(img_tag)}
 				<div class= "w-auto h-[300px] text-center line-[75px] font-[30px] ">
 					<a href="/portfolio/{slug}">
 						<div class = "relative">
