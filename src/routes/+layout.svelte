7d7fb53c9470fed9b14ac5569c6d70b54dd7c27d
Nicholas Ross <RossNicholasV@gmail.com>
2024-10-05 05:36:32 +0000

    Added tailwindcss-spring and animated nav buttons

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -22,19 +22,19 @@
 <nav class= "bg-[#333]">
 	<ul class= "list-none m-auto p-0 overflow-hidden w-[50vw]">
 		<li class= "float-left">
-			<a class="block text-white text-center px-[16px] py-[14px] hover:bg-[#111]" href="/" aria-current={$page.url.pathname === "/"}><h3>Home</h3></a>
+			<a class="block text-white text-center px-[16px] py-[14px] spring-bounce-60 spring-duration-300 transition-transform hover:scale-150 hover:bg-[#111]" href="/" aria-current={$page.url.pathname === "/"}><h3>Home</h3></a>
 		</li>
 
 		<li class= "float-left">
-			<a class="block text-white text-center px-[16px] py-[14px] hover:bg-[#111]" href="/portfolio" aria-current={$page.url.pathname === "/portfolio"}>Portfolio</a>
+			<a class="block text-white text-center px-[16px] py-[14px] spring-bounce-60 spring-duration-300 transition-transform hover:scale-150 hover:bg-[#111]" href="/portfolio" aria-current={$page.url.pathname === "/portfolio"}>Portfolio</a>
 		</li>
 
 		<!--<li class= "float-left">
-			<a class="block text-white text-center px-[16px] py-[14px] hover:bg-[#111]" href="/blog" aria-current={$page.url.pathname === "/blog"}>Blog</a>
+			<a class="block text-white text-center px-[16px] py-[14px] spring-bounce-60 spring-duration-300 transition-transform hover:scale-150 hover:bg-[#111]" href="/blog" aria-current={$page.url.pathname === "/blog"}>Blog</a>
 		</li>-->
 
 		<li class= "float-left">
-			<a class="block text-white text-center px-[16px] py-[14px] hover:bg-[#111]" href="/about" aria-current={$page.url.pathname === "/about"}>About</a>
+			<a class="block text-white text-center px-[16px] py-[14px] spring-bounce-60 spring-duration-300 transition-transform hover:scale-150 hover:bg-[#111]" href="/about" aria-current={$page.url.pathname === "/about"}>About</a>
 		</li>
 	</ul>
 </nav>
@@ -43,14 +43,14 @@
 
 <footer class="m-auto text-center justify-left w-50vw">
 	<!--social media links here-->
-	<a href="https://www.linkedin.com/in/nicholas-ross-a17023b0/" class="fa-brands p-[16px] text-[16px] w-[48px] h-[48px] text-center rounded-[50%] text-white bg-black hover:opacity-[50%] fa-linkedin" target="_blank">
+	<a href="https://www.linkedin.com/in/nicholas-ross-a17023b0/" class="fa-brands p-[16px] text-[16px] w-[48px] h-[48px] text-center rounded-[50%] text-white bg-black spring-bounce-60 spring-duration-300 transition-transform hover:scale-150 hover:opacity-[50%] fa-linkedin" target="_blank">
 	</a>
-	<a href="https://www.github.com/lealchemist" class="fa-brands p-[16px] text-[16px] w-[48px] h-[48px] text-center rounded-[50%] text-white bg-black hover:opacity-[50%] fa-github" target="_blank">
+	<a href="https://www.github.com/lealchemist" class="fa-brands p-[16px] text-[16px] w-[48px] h-[48px] text-center rounded-[50%] text-white bg-black spring-bounce-60 spring-duration-300 transition-transform hover:scale-150 hover:opacity-[50%] fa-github" target="_blank">
 	</a>
-	<a href="https://twitter.com/le_alchemist" class="fa-brands p-[16px] text-[16px] w-[48px] h-[48px] text-center rounded-[50%] text-white bg-black hover:opacity-[50%] fa-twitter" target="_blank">
+	<a href="https://twitter.com/le_alchemist" class="fa-brands p-[16px] text-[16px] w-[48px] h-[48px] text-center rounded-[50%] text-white bg-black spring-bounce-60 spring-duration-300 transition-transform hover:scale-150 hover:opacity-[50%] fa-twitter" target="_blank">
 	</a>
-	<a href="https://www.youtube.com/@le_alchemist" class="fa-brands p-[16px] text-[16px] w-[48px] h-[48px] text-center rounded-[50%] text-white bg-black hover:opacity-[50%] fa-youtube" target="_blank">
+	<a href="https://www.youtube.com/@le_alchemist" class="fa-brands p-[16px] text-[16px] w-[48px] h-[48px] text-center rounded-[50%] text-white bg-black spring-bounce-60 spring-duration-300 transition-transform hover:scale-150 hover:opacity-[50%] fa-youtube" target="_blank">
 	</a>
-	<a href="https://patreon.com" class="fa-brands p-[16px] text-[16px] w-[48px] h-[48px] text-center rounded-[50%] text-white bg-black hover:opacity-[50%] fa-patreon" target="_blank">
+	<a href="https://patreon.com" class="fa-brands p-[16px] text-[16px] w-[48px] h-[48px] text-center rounded-[50%] text-white bg-black spring-bounce-60 spring-duration-300 transition-transform hover:scale-150 hover:opacity-[50%] fa-patreon" target="_blank">
 	</a>
 </footer>
