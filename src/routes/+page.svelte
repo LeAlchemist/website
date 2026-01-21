71a0a86816fa05e72417141cacc0732df894561d
LeAlchemist <rossnicholasv@gmail.com>
2026-01-21 17:46:15 -0600

    Set up image and grid scaling for portfolio

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -13,8 +13,16 @@
 	<ul class="posts">
 		{#each data.posts as post}
 			<li class="post">
+				<a href={post.slug}
+					><img
+						src={post.preview}
+						class="img"
+						alt={'Preview image for ' + post.title}
+						width="100%"
+						height="100%"
+					/></a
+				>
 				<a href={post.slug} class="title">{post.title}</a>
-				<img src={post.preview} alt={'Preview image for ' + post.title} />
 				<p class="date">{formatDate(post.date)}</p>
 				<p class="description">{post.description}</p>
 			</li>
@@ -25,27 +33,53 @@
 <style>
 	.posts {
 		display: grid;
-		gap: var(--size-7);
+		grid-template-columns: auto;
+
+		@media (min-width: 48rem) {
+			grid-template-columns: auto auto;
+		}
+
+		@media (min-width: 64rem) {
+			grid-template-columns: auto auto auto;
+		}
+
+		@media (min-width: 80rem) {
+			grid-template-columns: auto auto auto auto;
+		}
 
 		.post {
-			max-inline-size: var(--size-content-3);
+			padding: 10px;
+			position: relative;
+			text-align: left;
 
-			&:not(:last-child) {
-				border-bottom: 1px solid var(--border);
-				padding-bottom: var(--size-7);
+			.img {
+				width: 100%;
+				height: 228px;
+				object-fit: cover;
 			}
 
 			.title {
-				font-size: var(--font-size-fluid-3);
+				position: absolute;
+				top: 8px;
+				font-size: var(--font-size-fluid-1);
+				color: white;
 				text-transform: capitalize;
+				background-color: rgba(48, 48, 48, 0.75);
 			}
 
 			.date {
-				color: var(--text-2);
+				position: absolute;
+				top: 50px;
+				color: white;
+				background-color: rgba(48, 48, 48, 0.75);
 			}
 
 			.description {
-				margin-top: var(--size-3);
+				position: absolute;
+				bottom: 8px;
+				margin-top: var(--size-1);
+				color: white;
+				background-color: rgba(48, 48, 48, 0.75);
 			}
 		}
 	}
