19c8d9532ed570fd6c5cd2c01034c05e687170f3
LeAlchemist <rossnicholasv@gmail.com>
2026-02-04 02:18:33 -0600

    Got Categories dropdown working for filtering images

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,6 +3,12 @@
 	import * as config from '$lib/config';
 
 	let { data } = $props();
+
+	let imgTag = $state('');
+
+	function tag(tag: string) {
+		imgTag = tag;
+	}
 </script>
 
 <svelte:head>
@@ -10,27 +16,81 @@
 </svelte:head>
 
 <section>
+	<ul class="dropdown">
+		<button class="dropbtn">Categories</button>
+		<div class="dropdown-content">
+			<button onclick={() => tag('')}>All</button>
+			<button onclick={() => tag('3D Art')}>3D Art</button>
+			<button onclick={() => tag('2D Art')}>2D Art</button>
+		</div>
+	</ul>
+
 	<ul class="posts">
 		{#each data.posts as post}
-			<li class="post">
-				<a href={post.slug}
-					><img
-						src={post.preview}
-						class="img"
-						alt={'Preview image for ' + post.title}
-						width="100%"
-						height="100%"
-					/></a
-				>
-				<a href={post.slug} class="title">{post.title}</a>
-				<p class="date">{formatDate(post.date)}</p>
-				<p class="description">{post.description}</p>
-			</li>
+			{#if post.categories.includes(imgTag)}
+				<li class="post">
+					<a href={post.slug}
+						><img
+							src={post.preview}
+							class="img"
+							alt={'Preview image for ' + post.title}
+							width="100%"
+							height="100%"
+						/></a
+					>
+					<a href={post.slug} class="title">{post.title}</a>
+					<p class="date">{formatDate(post.date)}</p>
+					<p class="description">{post.description}</p>
+				</li>
+			{:else if imgTag === ''}
+				<li class="post">
+					<a href={post.slug}
+						><img
+							src={post.preview}
+							class="img"
+							alt={'Preview image for ' + post.title}
+							width="100%"
+							height="100%"
+						/></a
+					>
+					<a href={post.slug} class="title">{post.title}</a>
+					<p class="date">{formatDate(post.date)}</p>
+					<p class="description">{post.description}</p>
+				</li>
+			{/if}
 		{/each}
 	</ul>
 </section>
 
 <style>
+	.dropdown {
+		position: relative;
+		z-index: 1;
+
+		.dropbtn {
+			padding: 16px;
+			font-size: 16px;
+			border: none;
+			cursor: pointer;
+		}
+
+		.dropdown-content {
+			display: none;
+			position: absolute;
+			min-width: 200px;
+
+			button {
+				padding: 12px 16px;
+				text-decoration: none;
+				display: block;
+			}
+		}
+	}
+
+	.dropdown:hover .dropdown-content {
+		display: block;
+	}
+
 	.posts {
 		display: grid;
 		grid-template-columns: auto;
